fae212585d20723b089c87fabe76759af12821b0
Edwin Bravo <87359741+Edw11n@users.noreply.github.com>
2025-03-06 12:56:21 -0500

    Update signupController.js

--- src/apis/signupController.js
+++ src/apis/signupController.js
@@ -3,6 +3,7 @@ const API_URL = process.env.REACT_APP_API_URL;
 
 export const signupUser = async (userData) => {
     try {
+        console.log("API_URL:", API_URL);
         await Axios.post(`${API_URL}/lessors/signup`, userData);
         return { success: true };
     } catch (error) {
