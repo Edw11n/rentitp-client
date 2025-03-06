ed7bc50cee72c8333606a2f253b9a076df9f3dbd
Edwin Bravo <87359741+Edw11n@users.noreply.github.com>
2025-03-06 12:28:25 -0500

    Update mapController.js

--- src/apis/mapController.js
+++ src/apis/mapController.js
@@ -4,6 +4,7 @@ const API_URL = process.env.REACT_APP_API_URL;
 // Función para obtener los apartamentos
 const fetchApartments = async () => {
     try {
+        console.log(`API_URL ${API_URL}`);
         const response = await axios.get(`${API_URL}/apartments/getMarkersInfo`);
         return response.data; // Retorna los datos de los apartamentos
     } catch (error) {
